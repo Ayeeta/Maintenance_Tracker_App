ba89f98d5c881c90bad0110e52bbff6487d3ceaf
Ayeeta <elijate@gmail.com>
2018-05-28 20:01:37 +0300

    Add dummy data to the request log

---
+++ static/js/requestlog.js
@@ -0,0 +1,10 @@
+function reqlog() {
+    var newdiv = document.createElement("div");
+
+    newdiv.style.width = "700px";
+    newdiv.style.marginTop = "10px";
+    newdiv.style.boxShadow = "0 0 5px #b3b3b3";
+    newdiv.style.padding = "10px";
+    newdiv.innerHTML = "<h3> <font color=#ff3333>&#9874; repair</font> - Cony Vaio S523d</h3> <p>Screen is broken</p> <p><font size=1 color=grey>12/05/2018</font></p>";
+    document.getElementById("parent-div").appendChild(newdiv);
+}
